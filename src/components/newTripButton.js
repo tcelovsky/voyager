f0dc7957479826a3e0f1fc09055f6cd5f74f14e7
tscotce <tatyana.celovsky@gmail.com>
2022-08-31 12:36:46 -0400

    Create basic component for button

--- src/components/newTripButton.js
+++ src/components/newTripButton.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 const NewTripButton = () => {
-  return <div className="new_trip_button">New Trip Button</div>;
+  return (
+    <div className="button">
+      <Button type="submit" variant="outline-secondary">
+        Create a New Trip
+      </Button>
+    </div>
+  );
 };
 
 export default NewTripButton;
