11e50137078a905e482524b39d2e21021b2ffc3c
tscotce <tatyana.celovsky@gmail.com>
2022-08-31 12:47:25 -0400

    Import button.css into the Button component

--- src/components/newTripButton.js
+++ src/components/newTripButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import "../css/button.css";
 
 const NewTripButton = () => {
   return (
     <div className="button">
-      <Button type="submit" variant="outline-secondary">
-        Create a New Trip
+      <Button type="submit" variant="outline-secondary" size="sm">
+        New Trip
       </Button>
     </div>
   );
